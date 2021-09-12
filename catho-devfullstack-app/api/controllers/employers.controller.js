278b95f334497c2a7440234518a7dbaf55b01627
Eduardo Galbiati [Caze] <eduardo.galbiati7@gmail.com>
2021-09-12 09:49:25 -0300

    fix(controller): remove elses and replace by an earlier return. Adds status code verb 404 to not found. TODO: we could throw an exception inside the try and validate the exception type into the controllers catch to keep all errors togheter

--- catho-devfullstack-app/api/controllers/employers.controller.js
+++ catho-devfullstack-app/api/controllers/employers.controller.js
@@ -21,11 +21,10 @@ const put = async (req, res) => {
   try {
     const employer = await employerService.update(id, req.body);
 
-    if (employer) {
-      res.status(201).send(`Employer ${id} updated successfully!`);
-    } else {
-      res.status(200).send(`Employer not found!`);
+    if (!employer) {
+      return res.status(404).send(`Employer not found!`);
     }
+    res.status(201).send(`Employer ${id} updated successfully!`);
   } catch (err) {
     res.status(400).send(err);
   }
@@ -41,11 +40,10 @@ const del = async (req, res) => {
   try {
     employer = await employerService.destroy(id);
 
-    if (employer) {
-      res.status(200).send(`Employer ${id} deleted successfully!`);
-    } else {
-      res.status(200).send(`Employer not found!`);
+    if (!employer) {
+      res.status(404).send(`Employer not found!`);
     }
+    res.status(200).send(`Employer ${id} deleted successfully!`);
   } catch (err) {
     res.status(400).send(err);
   }
@@ -65,11 +63,10 @@ const getById = async (req, res) => {
 
   try {
     employer = await employerService.getById(id);
-    if (employer) {
-      res.status(200).json(employer);
-    } else {
-      res.status(200).send(`Employer not found!`);
+    if (!employer) {
+      res.status(404).send(`Employer not found!`);
     }
+    res.status(200).json(employer);
   } catch (err) {
     res.status(400).send(err);
   }
